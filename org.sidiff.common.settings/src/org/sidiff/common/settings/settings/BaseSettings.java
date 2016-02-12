40b4b62e3cb35b961fe03feb35712fa6a4eeeed1
cpietsch <cpietsch@informatik.uni-siegen.de>
2016-02-12 07:19:15 +0100

    SiDiffSettings is called BaseSettings now

---
+++ org.sidiff.common.settings/src/org/sidiff/common/settings/settings/BaseSettings.java
@@ -0,0 +1,59 @@
+package org.sidiff.common.settings.settings;
+
+import org.sidiff.common.emf.access.Scope;
+
+public class BaseSettings extends AbstractSettings {
+	/**
+	 * The {@link Scope} of the comparison. (Default: {@link Scope#RESOURCE}.
+	 */
+	private Scope scope = Scope.RESOURCE;
+
+	
+	public BaseSettings() {
+		super();
+	}
+
+
+	public BaseSettings(Scope scope) {
+		this.scope=scope;
+	}
+	
+	@Override
+	public boolean validateSettings() {
+		return scope!=null;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		StringBuffer result = new StringBuffer();
+		result.append(getScope() != null ? "Scope: " + getScope() + "\n" : "");
+		
+		return result.toString();
+	}
+	
+	// ---------- Getter and Setter Methods----------
+	
+	/**
+	 * @return The {@link Scope} of the comparison.
+	 */
+	public Scope getScope() {
+		return scope;
+	}
+	
+	/**
+	 * Setup the new {@link Scope} of the comparison.
+	 * 
+	 * @param scope
+	 *            The new {@link Scope}.
+	 */
+	public void setScope(Scope scope) {
+		if (this.scope == null || !this.scope.equals(scope)) {
+			this.scope = scope;
+			this.notifyListeners(SiDiffSettingsItem.SCOPE);
+		}
+	}
+}

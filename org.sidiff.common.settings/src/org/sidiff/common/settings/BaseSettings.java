e7c67dfe66193248fdc579f787bf14521cf8a08c
Robert Müller <robert.mueller@student.uni-siegen.de>
2018-05-26 17:01:53 +0200

    refactor settings

--- org.sidiff.common.settings/src/org/sidiff/common/settings/BaseSettings.java
+++ org.sidiff.common.settings/src/org/sidiff/common/settings/BaseSettings.java
@@ -1,8 +1,15 @@
 package org.sidiff.common.settings;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.runtime.Status;
 import org.sidiff.common.emf.access.Scope;
 
+/**
+ * @see BaseSettingsItem
+ */
 public class BaseSettings extends AbstractSettings {
+
 	/**
 	 * The {@link Scope} of the comparison. (Default: {@link Scope#RESOURCE}.
 	 */
@@ -12,78 +19,77 @@ public class BaseSettings extends AbstractSettings {
 	 * Validation of the input models. (Default: False)
 	 */
 	private boolean validate = false;
-	
+
 	public BaseSettings() {
 		super();
 	}
 
-
 	public BaseSettings(Scope scope, boolean validate) {
+		super();
 		this.scope = scope;
 		this.validate = validate;
 	}
-	
+
 	@Override
-	public boolean validateSettings() {
-		return scope!=null;
+	protected void validate(MultiStatus multiStatus) {
+		if(scope == null) {
+			multiStatus.add(new Status(IStatus.ERROR, Activator.PLUGIN_ID, 0, "Scope is not set.", null));
+		}
 	}
-	
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
+
+	@Override
 	public String toString() {
-		StringBuffer result = new StringBuffer();
-		result.append(getScope() != null ? "Scope: " + getScope() + "\n" : "");
-		result.append("Validate input models: " + isValidate());
-		
-		return result.toString();
+		return new StringBuilder()
+			.append("BaseSettings[")
+			.append("Scope: ").append(getScope()).append(", ")
+			.append("Validate input models: ").append(isValidate())
+			.append("]")
+			.toString();
 	}
-	
+
 	// ---------- Getter and Setter Methods----------
-	
+
 	/**
 	 * @return The {@link Scope} of the comparison.
+	 * @see BaseSettingsItem#SCOPE
 	 */
 	public Scope getScope() {
 		return scope;
 	}
-	
+
 	/**
 	 * Setup the new {@link Scope} of the comparison.
 	 * 
-	 * @param scope
-	 *            The new {@link Scope}.
+	 * @param scope The new {@link Scope}.
+	 * @see BaseSettingsItem#SCOPE
 	 */
 	public void setScope(Scope scope) {
-		if (this.scope == null || !this.scope.equals(scope)) {
+		if (this.scope != scope) {
 			this.scope = scope;
-			this.notifyListeners(BaseSettingsItem.SCOPE);
+			notifyListeners(BaseSettingsItem.SCOPE);
 		}
 	}
-	
+
 	/**
 	 * Get the validation of the input models. (Default: False)
 	 * 
-	 * @return <code>true</code> if the input models will be validated;
-	 *         <code>false</code> otherwise.
+	 * @return <code>true</code> if the input models will be validated; <code>false</code> otherwise.
+	 * @see BaseSettingsItem#VALIDATE
 	 */
 	public boolean isValidate() {
 		return validate;
 	}
-	
+
 	/**
 	 * Set the validation of the input models. (Default: False)
 	 * 
-	 * @param validate
-	 *            <code>true</code> if the input models should be validated;
-	 *            <code>false</code> otherwise.
+	 * @param validate <code>true</code> if the input models should be validated; <code>false</code> otherwise.
+	 * @see BaseSettingsItem#VALIDATE
 	 */
 	public void setValidate(boolean validate) {
 		if (this.validate != validate) {
 			this.validate = validate;
-			this.notifyListeners(BaseSettingsItem.VALIDATE);
+			notifyListeners(BaseSettingsItem.VALIDATE);
 		}
 	}
 }

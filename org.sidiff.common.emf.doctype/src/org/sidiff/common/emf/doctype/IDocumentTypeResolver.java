5d8573c9e4e38b474574ae645b3e83c2a6f38f87
Robert Müller <robert.mueller@student.uni-siegen.de>
2018-06-08 15:54:46 +0200

    update document type resolver

--- org.sidiff.common.emf.doctype/src/org/sidiff/common/emf/doctype/IDocumentTypeResolver.java
+++ org.sidiff.common.emf.doctype/src/org/sidiff/common/emf/doctype/IDocumentTypeResolver.java
@@ -4,13 +4,28 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.resource.Resource;
 
-
+/**
+ * An <code>IDocumentTypeResolver</code> resolves the document types
+ * of {@link Resource}s for models with a specific document type.
+ * @author cpietsch, Robert Müller
+ *
+ */
 public interface IDocumentTypeResolver {
 
-	public static String extensionPointID = "org.sidiff.common.emf.doctype.document_type_resolver";
-	public static String attribute_class = "class";
+	public static final String extensionPointID = "org.sidiff.common.emf.doctype.document_type_resolver";
+	public static final String attribute_class = "class";
 
+	/**
+	 * Resolve the document types of the given resource. May return
+	 * <code>null</code> if no document types could be resolved.
+	 * @param resource the resource
+	 * @return list of document types
+	 */
 	public List<String> resolve(Resource resource);
 
+	/**
+	 * Returns the type of documents that this resolver can handle.
+	 * @return document type
+	 */
 	public String getDocumentType();
 }
--- org.sidiff.common.emf.doctype/src/org/sidiff/common/emf/doctype/util/EMFDocumentTypeUtil.java
+++ org.sidiff.common.emf.doctype/src/org/sidiff/common/emf/doctype/util/EMFDocumentTypeUtil.java
@@ -1,8 +1,14 @@
 package org.sidiff.common.emf.doctype.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
@@ -12,67 +18,86 @@ import org.sidiff.common.emf.access.Scope;
 import org.sidiff.common.emf.doctype.IDocumentTypeResolver;
 
 /**
- * 
- * @author cpietsch
+ * <code>EMFDocumentTypeUtil</code> contains utility functions for resolving
+ * the document types of {@link Resource}s.
+ * @author cpietsch, Robert Müller
  *
  */
 public class EMFDocumentTypeUtil {
-	
-	public static List<String> resolve(Resource resource) {
-		List<String> docTypes = new ArrayList<String>();
-		
-		// try to resolve document types via registered IDocumentTypeResolvers
-		//
-		for(IDocumentTypeResolver resolver : getAvailableDocumentTypeResolvers()) {
-			docTypes.addAll(resolver.resolve(resource));
-			if(!docTypes.isEmpty()) break;
-		}
-		
-		// if no document types are resolved use the document types of the resource
-		//
-		if(docTypes.isEmpty()) {
-			docTypes.addAll(EMFModelAccess.getDocumentTypes(resource, Scope.RESOURCE_SET));
-			
-		}
-		
-		docTypes.sort(new Comparator<String>() {
 
+	/**
+	 * Returns the resolved document types of the given resource.
+	 * @param resource the resource
+	 * @return list of document types
+	 */
+	public static List<String> resolve(Resource resource) {
+		return resolve(Collections.singleton(resource));
+	}
+
+	/**
+	 * Returns the resolved document types of the given resources.
+	 * @param resources the resources
+	 * @return list of document types
+	 */
+	public static List<String> resolve(Collection<Resource> resources) {
+		Set<String> docTypes = new HashSet<String>();
+		Map<String, IDocumentTypeResolver> resolvers = getAvailableDocumentTypeResolvers();
+
+		resourceLoop:
+		for(Resource resource : resources) {
+			if(resource == null || resource.getContents().isEmpty()) {
+				continue;
+			}
+
+			Set<String> modelDocTypes = EMFModelAccess.getDocumentTypes(resource, Scope.RESOURCE_SET);
+
+			// try to resolve document types via registered IDocumentTypeResolvers
+			//
+			for(String docType : modelDocTypes) {
+				IDocumentTypeResolver resolver = resolvers.get(docType);
+				if(resolver != null) {
+					List<String> resolvedTypes = resolver.resolve(resource);
+					if(resolvedTypes != null) {
+						docTypes.addAll(resolvedTypes);
+						continue resourceLoop; // continue with next resource
+					}
+				}
+			}
+
+			// if no document types are resolved, use the document types of the resource
+			//
+			docTypes.addAll(modelDocTypes);
+		}
+
+		// create and sort list of document types
+		List<String> docTypesList = new ArrayList<>(docTypes);
+		docTypesList.sort(new Comparator<String>() {
 			@Override
 			public int compare(String o1, String o2) {
 				return o1.compareTo(o2);
 			}
 		});
-		
-		return docTypes;
+		return docTypesList;
 	}
 
 	/**
-	 * 
-	 * @return
+	 * Returns all available document type resolvers, as a map of document type -> resolver.
+	 * @return all available document type resolvers
 	 */
-	private static List<IDocumentTypeResolver> getAvailableDocumentTypeResolvers(){
-		List<IDocumentTypeResolver> resolver = new ArrayList<IDocumentTypeResolver>();
+	protected static Map<String, IDocumentTypeResolver> getAvailableDocumentTypeResolvers() {
+		Map<String, IDocumentTypeResolver> resolvers = new HashMap<>();
 
 		for (IConfigurationElement configurationElement : Platform.getExtensionRegistry().getConfigurationElementsFor(
 				IDocumentTypeResolver.extensionPointID)) {
 			try {
-				IDocumentTypeResolver resolverExtension = (IDocumentTypeResolver) configurationElement.createExecutableExtension(IDocumentTypeResolver.attribute_class);
-				resolver.add(resolverExtension);
-				
+				IDocumentTypeResolver resolverExtension = (IDocumentTypeResolver) configurationElement.createExecutableExtension(
+						IDocumentTypeResolver.attribute_class);
+				resolvers.put(resolverExtension.getDocumentType(), resolverExtension);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
-		resolver.sort(new Comparator<IDocumentTypeResolver>() {
 
-			@Override
-			public int compare(IDocumentTypeResolver arg0, IDocumentTypeResolver arg1) {
-				
-				return arg0.getDocumentType().compareTo(arg1.getDocumentType());
-			}
-			
-		});
-		return resolver;
+		return resolvers;
 	}
 }

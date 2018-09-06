d8d3afd55e264250276b2c6a29e685c042fa031a
Robert Müller <robert.mueller@student.uni-siegen.de>
2018-09-07 00:08:21 +0200

    add javadoc to label printer

--- org.sidiff.common.stringresolver/src/org/sidiff/common/stringresolver/util/LabelPrinter.java
+++ org.sidiff.common.stringresolver/src/org/sidiff/common/stringresolver/util/LabelPrinter.java
@@ -6,32 +6,61 @@ import org.sidiff.common.emf.access.EMFModelAccess;
 import org.sidiff.common.stringresolver.GenericStringResolver;
 import org.sidiff.common.stringresolver.IStringResolver;
 
+/**
+ * The label printer provides human readable labels
+ * and tooltips using the available {@link IStringResolver}s.
+ */
 public class LabelPrinter {
 
 	private final IStringResolver resolver;
 
+	/**
+	 * Creates a label printer using a generic string resolver.
+	 */
 	public LabelPrinter() {
 		this.resolver = new GenericStringResolver();
 	}
 
+	/**
+	 * Creates a label printer using the string resolver for the
+	 * characteristic document type of the given model, or a
+	 * generic string resolver if no alternative was found.
+	 * @param model the model resource
+	 */
 	public LabelPrinter(Resource model) {
 		this(EMFModelAccess.getCharacteristicDocumentType(model));
 	}
 
+	/**
+	 * Creates a label printer using the string resolver for the
+	 * given document type, or a generic string resolver if no
+	 * alternative was found.
+	 * @param docType the document type
+	 */
 	public LabelPrinter(String docType) {
 		this.resolver = IStringResolver.MANAGER.getDefaultExtension(docType).orElseGet(GenericStringResolver::new);
 	}
 
-	public String getLabel(Object object){
+	/**
+	 * Returns a label for the given EObject.
+	 * @param object the object
+	 * @return label, "null" if null
+	 */
+	public String getLabel(EObject object){
 		if (object != null) {
-			return resolver.resolve((EObject) object);
+			return resolver.resolve(object);
 		}
 		return "null";
 	}
-	
-	public String getToolTipLabel(Object object){
+
+	/**
+	 * Returns a tooltip for the given EObject.
+	 * @param object the object
+	 * @return tooltip, "null" if null
+	 */
+	public String getToolTipLabel(EObject object){
 		if (object != null) {
-			return resolver.resolveQualified((EObject) object);
+			return resolver.resolveQualified(object);
 		}
 		return "null";
 	}

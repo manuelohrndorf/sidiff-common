11a957656387369d7de872a77f1246b8384c7410
Robert Müller <robert.mueller@student.uni-siegen.de>
2018-12-31 13:26:39 +0100

    add setSelection and getSelectableValues to IWidgetModification

--- org.sidiff.common.ui/src/org/sidiff/common/ui/widgets/IWidgetModification.java
+++ org.sidiff.common.ui/src/org/sidiff/common/ui/widgets/IWidgetModification.java
@@ -12,15 +12,34 @@ import java.util.List;
 public interface IWidgetModification<T> {
 
 	/**
-	 * Returns the selection of the widget.
-	 * Note that this method should return all selected values
-	 * and not aggregate them in any way. The list may also be
-	 * empty or only have one element for widgets that only
-	 * have a single value.
+	 * <p>Returns the selection of the widget.</p>
+	 * <p>Note that this method should return all selected values
+	 * directly and not return aggregates (e.g. incremental matchers etc.).</p>
+	 * <p>The list may also be empty or only have one element for widgets
+	 * that only have a single value.</p>
 	 * @return the selection
 	 */
 	List<T> getSelection();
 
+	/**
+	 * <p>Sets the selection of the widget.</p>
+	 * <p>The selection should contain the values directly and not
+	 * contain aggregate values (e.g. incremental matchers etc.).</p>
+	 * <p>The list may also be empty or only have one element for widgets
+	 * that only have a single value.</p>
+	 * @param selection
+	 * @throws IllegalArgumentException if the selection is not legal
+	 */
+	void setSelection(List<T> selection);
+
+	/**
+	 * <p>Returns all values that can be selected in this widget.</p>
+	 * <p>Returns an empty list if it would not make sense to
+	 * list all values, e.g. for primitive types.</p>
+	 * @return all selectable values
+	 */
+	List<T> getSelectableValues();
+	
 	/**
 	 * Adds a {@link ModificationListener} to this widget.
 	 * @param listener the listener

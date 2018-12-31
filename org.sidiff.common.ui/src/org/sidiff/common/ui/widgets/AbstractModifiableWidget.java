c08a271ee3a462e1dd46130e398c458153dba761
Robert Müller <robert.mueller@student.uni-siegen.de>
2018-12-31 16:32:38 +0100

    override get/setSelection in abstarct modifiable widget

--- org.sidiff.common.ui/src/org/sidiff/common/ui/widgets/AbstractModifiableWidget.java
+++ org.sidiff.common.ui/src/org/sidiff/common/ui/widgets/AbstractModifiableWidget.java
@@ -8,15 +8,44 @@ import java.util.Objects;
 
 /**
  * <p>Abstract widget with type argument to also implement {@link IWidgetModification}.</p>
- * <p>Implements a collection of modification listeners and provides the functions
- * {@link #propagateValueChange(List, List)} and {@link #propagateValueChange(Object, Object)}
- * which should be called after the value of the widget changes.</p>
+ * <p>Implements a list of selected values, a collection of modification listeners, and
+ * functions to propagate changes to the listeners. {@link #setSelection(List)} is
+ * implemented to propagate changes. Override {@link #hookSetSelection()} to implement
+ * custom handling for selections.</p>
  * @author Robert Müller
  */
 public abstract class AbstractModifiableWidget<T> extends AbstractWidget implements IWidgetModification<T>  {
 
+	private List<T> selection = Collections.emptyList();
 	private Collection<ModificationListener<T>> modificationListeners = new ArrayList<>();
 
+	@Override
+	public void setSelection(List<T> selection) {
+		if(!Objects.equals(selection, this.selection)) {
+			List<T> oldSelection = this.selection;
+			this.selection = new ArrayList<>(selection);
+			hookSetSelection();
+			propagateValueChange(oldSelection, this.selection);
+		}
+	}
+
+	/**
+	 * <p>Hook method called by {@link #setSelection(List)} after the selection
+	 * has been applied and before the new value has been propagated.
+	 * Use {@link #getSelection()} to get the current selection.
+	 * <u>Do not</u> call {@link #setSelection(List)} from this method.<p>
+	 * <p>Override to implement custom handling for new selections.
+	 * The default implementation does nothing.</p>
+	 */
+	protected void hookSetSelection() {
+		// default implementation does nothing
+	}
+
+	@Override
+	public List<T> getSelection() {
+		return Collections.unmodifiableList(selection);
+	}
+	
 	@Override
 	public void addModificationListener(ModificationListener<T> listener) {
 		modificationListeners.add(listener);

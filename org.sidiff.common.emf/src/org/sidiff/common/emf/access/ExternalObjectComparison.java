1a60871526f417a92bdb14a5a55df2a4ffc1b575
Robert Müller <robert.mueller@student.uni-siegen.de>
2019-12-03 13:17:15 +0100

    remove usage of deprecated method, reformat

--- org.sidiff.common.emf/src/org/sidiff/common/emf/access/ExternalObjectComparison.java
+++ org.sidiff.common.emf/src/org/sidiff/common/emf/access/ExternalObjectComparison.java
@@ -2,47 +2,49 @@ package org.sidiff.common.emf.access;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.sidiff.common.emf.EMFUtil;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
 /**
- * Utility to checks if two external elements are equal. 
- * They are considered equal, if they are both external and have the same URI.
- * Otherwise they are considered different. 
+ * Utility to checks if two external elements are equal. They are considered
+ * equal, if they are both external and have the same URI. Otherwise they are
+ * considered different.
  * 
- * Elements are considered to be internal, if they belong to one of the given resources.
+ * Elements are considered to be internal, if they belong to one of the given
+ * resources.
  * 
  * @author wenzel
  *
  */
 public enum ExternalObjectComparison {
 
-	Equal,
-	Different,
-	BothInternal;
-	
+	Equal, Different, BothInternal;
+
 	public static ExternalObjectComparison compare(EObject obj1, EObject obj2, Resource... resources) {
-		assert(resources.length>0):"No resources given!";
+		assert (resources.length > 0) : "No resources given!";
 		boolean ext1 = true;
 		boolean ext2 = true;
-		
-		for (int i = 0; i<resources.length; i++) {
-			if (ext1 && obj1.eResource()==resources[i]) {
+
+		for (int i = 0; i < resources.length; i++) {
+			if (ext1 && obj1.eResource() == resources[i]) {
 				ext1 = false;
 			}
-			if (ext2 && obj2.eResource()==resources[i]) {
+			if (ext2 && obj2.eResource() == resources[i]) {
 				ext2 = false;
 			}
-			if (!ext1 && !ext2)
+			if (!ext1 && !ext2) {
 				break;
+			}
 		}
-		
-		if (ext1 && ext2 && EMFUtil.getEObjectURI(obj1).equals(EMFUtil.getEObjectURI(obj2)))
+
+		if (ext1 && ext2 && EcoreUtil.getURI(obj1).equals(EcoreUtil.getURI(obj2))) {
 			return Equal;
-		
-		if (!ext1 && !ext2)
+		}
+
+		if (!ext1 && !ext2) {
 			return BothInternal;
-		
+		}
+
 		return Different;
 	}
-	
+
 }

30c45d89844e37864dd8a6c75dd9dcfff6f1301f
Robert Müller <robert.mueller@student.uni-siegen.de>
2020-03-05 17:03:11 +0100

    add MetricsListDifference.exportAsCsv

--- metrics/org.sidiff.common.emf.metrics/src/org/sidiff/common/emf/metrics/MetricsListDifference.java
+++ metrics/org.sidiff.common.emf.metrics/src/org/sidiff/common/emf/metrics/MetricsListDifference.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
+import org.sidiff.common.file.CSVWriter;
 
 public class MetricsListDifference extends ArrayList<MetricHandleDifference> {
 
@@ -28,5 +29,26 @@ public class MetricsListDifference extends ArrayList<MetricHandleDifference> {
 	public void recomputeAll(IProgressMonitor monitor) {
 		SubMonitor progress = SubMonitor.convert(monitor, size());
 		forEach(difference -> difference.recompute(progress.split(1)));
+	}
+
+	/**
+	 * Exports the differences in this list in CSV format, as
+	 * "<code>metric-key;origin-uri;change-uri;comparison-result</code>".
+	 * Only differences which have been computed and have a result before will be exported.
+	 * @return differences in CSV format
+	 */
+	public String exportAsCsv() {
+		return CSVWriter.writeToString(csvWriter -> {
+			csvWriter.setColumnSeparator(";");
+			for(MetricHandleDifference handle : this) {
+				if(handle.hasResults()) {
+					csvWriter.write(
+						handle.getMetric().getKey(),
+						handle.getOrigin().getContextLabel(),
+						handle.getChanged().getContextLabel(),
+						MetricsUtil.getLabel(handle.getResults()));
+				}
+			}
+		});
 	}
 }

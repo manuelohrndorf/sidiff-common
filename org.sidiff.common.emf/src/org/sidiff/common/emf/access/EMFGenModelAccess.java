3fa9c2836306fb4f816452b034903b626e0d60c8
Robert Müller <robert.mueller@student.uni-siegen.de>
2020-06-13 12:10:36 +0200

    add EMFGenModelAccess.getFileExtension

--- org.sidiff.common.emf/src/org/sidiff/common/emf/access/EMFGenModelAccess.java
+++ org.sidiff.common.emf/src/org/sidiff/common/emf/access/EMFGenModelAccess.java
@@ -1,8 +1,8 @@
 package org.sidiff.common.emf.access;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
 import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
@@ -11,35 +11,42 @@ import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.sidiff.common.emf.modelstorage.SiDiffResourceSet;
 
 /**
- * 
  * @author cpietsch
- *
+ * @author rmueller
  */
 public class EMFGenModelAccess {
 
 	/**
 	 * Returns all resource file extensions for a given set of document types.
 	 * 
-	 * @param nsURI
+	 * @param nsURIs
 	 *            The namespace URI(s) representing the document type(s) of a
 	 *            resource
-	 * @return file extensions for the given document type
+	 * @return file extensions for the given document types
 	 */
 	public static Set<String> getFileExtensions(Set<String> nsURIs) {
-		Set<String> fileExtensions = new HashSet<>();
+		return internalGetFileExtensions(nsURIs).collect(Collectors.toSet());
+	}
+
+	/**
+	 * Returns the first available file extension for the given set of document types.
+	 * Falls back to extension "xmi" if nothing else available.
+	 * @param nsURIs
+	 *            The namespace URI(s) representing the document type(s) of a
+	 *            resource
+	 * @return first available file extension for the document types, or "xmi"
+	 */
+	public static String getFileExtension(Set<String> nsURIs) {
+		return internalGetFileExtensions(nsURIs).findFirst().orElse("xmi");
+	}
+
+	private static Stream<String> internalGetFileExtensions(Set<String> nsURIs) {
 		SiDiffResourceSet resourceSet = SiDiffResourceSet.create();
 		Map<String, URI> nsURIMap = EcorePlugin.getEPackageNsURIToGenModelLocationMap(false);
-		for (String nsURI : nsURIs) {
-			URI uriGenModel = nsURIMap.get(nsURI);
-			if (uriGenModel != null) {
-				GenModel genModel = resourceSet.loadEObject(uriGenModel, GenModel.class);
-				if (genModel != null) {
-					for (GenPackage genPackage : genModel.getGenPackages()) {
-						fileExtensions.addAll(genPackage.getFileExtensionList());
-					}
-				}
-			}
-		}
-		return fileExtensions;
+		return nsURIs.stream()
+			.map(nsURIMap::get).filter(Objects::nonNull)
+			.map(uri -> resourceSet.loadEObject(uri, GenModel.class)).filter(Objects::nonNull)
+			.map(GenModel::getGenPackages).flatMap(Collection::stream)
+			.map(GenPackage::getFileExtensionList).flatMap(Collection::stream);
 	}
 }

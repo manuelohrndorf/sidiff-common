0de799d41c3e6a9478f7bad792b321a81981ac6d
Robert Müller <robert.mueller@student.uni-siegen.de>
2020-09-21 19:35:30 +0200

    make EMFValidate non-static to avoid interferences in the configuration

--- org.sidiff.common.emf/src/org/sidiff/common/emf/EMFValidate.java
+++ org.sidiff.common.emf/src/org/sidiff/common/emf/EMFValidate.java
@@ -15,12 +15,20 @@ import org.sidiff.common.logging.LogUtil;
 
 public class EMFValidate {
 
-	private static int minimumSeverity = Diagnostic.WARNING;
+	private int minimumSeverity;
 
-	public static void validateObject(EObject... eObjects) throws InvalidModelException {
-		LogUtil.log(LogEvent.NOTICE, "------------------------------------------------------------");
-		LogUtil.log(LogEvent.NOTICE, "-------------------------- Validate ------------------------");
-		LogUtil.log(LogEvent.NOTICE, "------------------------------------------------------------");
+	public EMFValidate() {
+		this(Diagnostic.WARNING);
+	}
+
+	public EMFValidate(int minimumSeverity) {
+		this.minimumSeverity = minimumSeverity;
+	}
+
+	public void validateObject(EObject... eObjects) throws InvalidModelException {
+		LogUtil.log(LogEvent.NOTICE, "----------------------------------------------------------");
+		LogUtil.log(LogEvent.NOTICE, "------------------------ Validate ------------------------");
+		LogUtil.log(LogEvent.NOTICE, "----------------------------------------------------------");
 
 		StringBuilder builder = new StringBuilder();
 		for (EObject eObject : eObjects) {
@@ -78,13 +86,13 @@ public class EMFValidate {
 	/**
 	 * Convenient method for validation a Resource, this just delegates the
 	 * validation for each root node of the resource.
-	 * 
+	 *
 	 * @param model
 	 *            resource to validate
 	 * @throws InvalidModelException
 	 *             if there are some negative validation results
 	 */
-	public static void validateModel(Resource model) throws InvalidModelException {
+	public void validateModel(Resource model) throws InvalidModelException {
 		for (EObject root : model.getContents()) {
 			validateObject(root);
 		}
@@ -93,30 +101,15 @@ public class EMFValidate {
 	/**
 	 * Convenient method for validation a ResourceSet, this just delegates the
 	 * validation for each root Resource of the ResourceSet.
-	 * 
+	 *
 	 * @param model
 	 *            resource set to validate
 	 * @throws InvalidModelException
 	 *             if there are some negative validation results
 	 */
-	public static void validateModel(ResourceSet model) throws InvalidModelException {
+	public void validateModel(ResourceSet model) throws InvalidModelException {
 		for (Resource root : model.getResources()) {
 			validateModel(root);
 		}
-
-	}
-
-	/**
-	 * Sets the minimum severity which is needed for throwing
-	 * an @{InvalidModelException}. If not set, defaults to @{Diagnostic.Warning}
-	 * and thus includes warnings as well as errors according to EMF Diagnostic.
-	 * @param severity
-	 */
-	public static void setMinimumSeverity(int severity) {
-		minimumSeverity = severity;
-	}
-
-	public static int getMinimumSeverity() {
-		return minimumSeverity;
 	}
 }

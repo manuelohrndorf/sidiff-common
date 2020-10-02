8717b7d20522e82c2e20e7992041cf7661dd3961
cpietsch <cpietsch@informatik.uni-siegen.de>
2020-10-02 13:30:06 +0200

    further metric for emf models

---
+++ metrics/org.sidiff.common.emf.metrics/src/org/sidiff/common/emf/metrics/defaults/ElementCountingMetric.java
@@ -0,0 +1,18 @@
+package org.sidiff.common.emf.metrics.defaults;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.sidiff.common.collections.CollectionUtil;
+import org.sidiff.common.emf.metrics.IMetricValueAcceptor;
+
+public class ElementCountingMetric extends AbstractResourceMetric {
+
+	@Override
+	protected void doCalculate(Resource context, IMetricValueAcceptor acceptor, IProgressMonitor monitor) {
+		acceptor.accept(
+				CollectionUtil.asStream(EcoreUtil.getAllProperContents(context, true)).count());
+
+	}
+
+}
